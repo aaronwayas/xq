c261bbac02028233c0b81ee6e5cdfbbc6b29d85c
aaronwayas <avazquezwayas@gmail.com>
2024-05-09 19:49:03 -0600

    asda

--- test.py
+++ test.py
@@ -1,58 +1,113 @@
 import os
 import platform
-import psutil
-from colorama import Style, Fore
 import calendar
 import datetime
-from cpuinfo import get_cpu_info as cpu_info
+import json
 
-VERSION = "1.0.0v"
+import psutil
+import argparse
+from colorama import Style, Fore
+from cpuinfo import get_cpu_info as cpuinfo
 
-# Configurations (Comming soon!)
+VERSION = "v0.1"
+DATA_FILE = "data.json"
+
+# Funciones para obtener información del sistema
 
 
-# User Info
-name_machine = Fore.RED + str(os.getenv("COMPUTERNAME")).lower() + Style.RESET_ALL
-user_name = Fore.RED + os.getenv("USERNAME") + Style.RESET_ALL
-user_plus_machine = f"{name_machine}@{user_name}"
-cpuInfo = cpu_info()["brand_raw"]
+def get_user_info() -> str:
+    """
+    Returns a string with user information.
+    """
+    name_machine = (
+        load_color() + str(os.getenv("COMPUTERNAME")).lower() + Style.RESET_ALL
+    )
+    user_name = load_color() + os.getenv("USERNAME") + Style.RESET_ALL
+    user_plus_machine = f"{name_machine}@{user_name}"
+
+    return user_plus_machine
 
 
-# All Colors
-def get_color_items():
+def get_cpu_info() -> str:
+    """
+    Returns a string with CPU information.
+    """
+
+    # Verifica si existe el archivo de datos
+    if not os.path.exists(DATA_FILE):
+        # Si el archivo de datos no existe, obtén la información de la CPU y guárdala en el archivo de datos
+        cpu_info = cpuinfo()["brand_raw"]
+        data = {"cpu_info": cpu_info}
+        with open(DATA_FILE, "w") as file:
+            json.dump(data, file)
+        return cpu_info
+
+    # Carga los datos del archivo de datos
+    with open(DATA_FILE, "r") as file:
+        try:
+            data = json.load(file)
+        except json.decoder.JSONDecodeError:
+            # Si el archivo está vacío o no es JSON válido, obtén la información de la CPU y guárdala en el archivo de datos
+            cpu_info = cpuinfo()["brand_raw"]
+            data = {"cpu_info": cpu_info}
+            with open(DATA_FILE, "w") as new_file:
+                json.dump(data, new_file)
+            return cpu_info
+
+    # Verifica si la información de la CPU está en los datos
+    if "cpu_info" in data:
+        return data["cpu_info"]
+
+    # Si la información de la CPU no está en los datos, obténla usando el módulo cpuinfo
+    cpu_info = cpuinfo()["brand_raw"]
+
+    # Guarda la información de la CPU en el archivo de datos
+    data["cpu_info"] = cpu_info
+    with open(DATA_FILE, "w") as file:
+        json.dump(data, file)
+
+    # Devuelve la información de la CPU
+    return cpu_info
+
+
+def get_color_items() -> list[str]:
+    """
+    Returns a list of color items. (Print "██")
+    """
     colors = [
-        Fore.BLACK,
-        Fore.RED,
-        Fore.GREEN,
-        Fore.YELLOW,
-        Fore.BLUE,
-        Fore.MAGENTA,
-        Fore.CYAN,
-        Fore.WHITE,
+        Fore.BLACK,  # negro
+        Fore.RED,  # rojo
+        Fore.GREEN,  # verde
+        Fore.YELLOW,  # amarillo
+        Fore.BLUE,  # azul
+        Fore.MAGENTA,  # magenta
+        Fore.CYAN,  # cian
+        Fore.WHITE,  # blanco
     ]
+
     color_items = [c + "██" + Style.RESET_ALL for c in colors]
     return color_items
 
 
-# OS Info
-os_info = (
-    platform.system()
-    + " "
-    + platform.release()
-    + f" ({platform.version()})"
-    + " "
-    + "x"
-    + platform.architecture()[0]
-)
-
-# Ram Info
-ram_used = round(psutil.virtual_memory().used / (1024.0**3), 2)
-ram_total = round(psutil.virtual_memory().total / (1024.0**3), 2)
-Ram_info = f"{ram_used}/ {ram_total} GB"
+def get_os_info() -> str:
+    """
+    Returns a string with OS information.
+    """
+    return (
+        platform.system()
+        + " "
+        + platform.release()
+        + f" ({platform.version()})"
+        + " "
+        + "x"
+        + platform.architecture()[0]
+    )
 
 
-# Uptime
-def get_uptime_info():
+def get_uptime_info() -> str:
+    """
+    Returns a string with system uptime.
+    """
     boot_time_timestamp = psutil.boot_time()
     boot_time_datetime = datetime.datetime.fromtimestamp(boot_time_timestamp)
     now = datetime.datetime.now()
@@ -68,30 +123,155 @@ def get_uptime_info():
     return uptime_str
 
 
-# Calendar
-now_month = datetime.datetime.now().month
-now_year = datetime.datetime.now().year
-calendar_month = calendar.month(now_year, now_month, 0, 0)
+def get_calendar_info() -> str:
+    """
+    Returns a string with calendar information.
+    """
+    now_month = datetime.datetime.now().month
+    now_year = datetime.datetime.now().year
+    calendar_month = calendar.month(now_year, now_month, 0, 0)
 
-date_today = (
-    f"{datetime.date.today()}, {datetime.datetime.now().strftime('%I:%M:%S %p')}"
+    date_today = (
+        f"{datetime.date.today()}, {datetime.datetime.now().strftime('%I:%M:%S %p')}"
+    )
+
+    return calendar_month, date_today
+
+
+def get_ram_info() -> str:
+    """
+    Returns a string with RAM information.
+    """
+    ram_used = round(psutil.virtual_memory().used / (1024.0**3), 2)
+    ram_total = round(psutil.virtual_memory().total / (1024.0**3), 2)
+    ram_info = f"{ram_used}/ {ram_total} GB"
+    return ram_info
+
+
+def load_color():
+    """
+    Load the color data from the JSON file.
+    """
+
+    with open(DATA_FILE, "r") as file:
+        data = json.load(file)
+        data = data["color"].lower()
+
+    if data:
+        return getattr(Fore, data.upper())
+
+    return Fore.WHITE
+
+
+def fetch_data():
+    os = f"{load_color()}OS{Style.RESET_ALL}: \t{get_os_info()}"
+    uptime = f"{load_color()}Uptime{Style.RESET_ALL}:\t{get_uptime_info()}"
+    cpu = f"{load_color()}CPU{Style.RESET_ALL}:\t{get_cpu_info()}"
+    ram = f"{load_color()}RAM{Style.RESET_ALL}:\t{get_ram_info()}"
+    date = f"{load_color()}Date{Style.RESET_ALL}:\t{get_calendar_info()[1]}"
+    xq = f"{load_color()}XQ{Style.RESET_ALL}:\t{VERSION}"
+
+    return os, uptime, cpu, ram, date, xq
+
+
+def fetch_info():
+    """
+    Returns a string with all system information.
+    """
+
+    return f"""
+
+            {Fore.YELLOW}.++######++.                       
+         .################+                         {get_user_info()}
+        {Fore.YELLOW} +################-{Style.RESET_ALL}                         {"-" * 26}
+         {Fore.YELLOW}#################{Style.RESET_ALL}   {Fore.GREEN}###+            +-     {fetch_data()[0]}
+        {Fore.YELLOW}#################-{Style.RESET_ALL} {Fore.GREEN} .#################      {fetch_data()[1]}
+       {Fore.YELLOW}-#################{Style.RESET_ALL}  {Fore.GREEN} #################.      {fetch_data()[2]}
+       {Fore.YELLOW}#################{Style.RESET_ALL}   {Fore.GREEN}.#################       {fetch_data()[3]}
+      {Fore.YELLOW}+################+{Style.RESET_ALL}   {Fore.GREEN}#################-       {fetch_data()[4]}
+      {Fore.YELLOW}###+        +####.{Style.RESET_ALL}  {Fore.GREEN}.#################        {fetch_data()[5]}
+      {Fore.YELLOW}                .{Style.RESET_ALL}   {Fore.GREEN}#################.        
+      {Fore.BLUE}-+##########+{Style.RESET_ALL}      {Fore.GREEN}##################         
+    {Fore.BLUE}+#################     {Fore.GREEN}.############+.     
+    {Fore.BLUE}#################   {Style.BRIGHT}{Fore.YELLOW}.{Style.RESET_ALL}                      
+   {Fore.BLUE}#################.      {Style.BRIGHT}{Fore.YELLOW}#####.       .###        {Style.DIM}{''.join(get_color_items())}
+  {Fore.BLUE}.#################      {Style.BRIGHT}{Fore.YELLOW}+################+        {Style.BRIGHT}{''.join(get_color_items())}
+  {Style.RESET_ALL}{Fore.BLUE}+################.      {Style.BRIGHT}{Fore.YELLOW}#################        
+ {Style.RESET_ALL}{Fore.BLUE}.#################      {Style.BRIGHT}{Fore.YELLOW}#################-       
+ {Style.RESET_ALL}{Fore.BLUE}#################-     {Style.BRIGHT}{Fore.YELLOW}-#################        
+{Style.RESET_ALL}{Fore.BLUE}-#.          -####  {Style.BRIGHT}{Fore.YELLOW}   #################.        
+                       {Style.BRIGHT}{Fore.YELLOW}.################+         
+                       {Style.BRIGHT}{Fore.YELLOW}.################.         
+                          {Style.BRIGHT}{Fore.YELLOW}.+#######++.{Style.RESET_ALL}     
+   
+    """
+
+
+# Funciones para manipular argumentos de línea de comandos
+
+
+def delete_data():
+    """
+    Function to delete data.
+    """
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "w") as file:
+            file.truncate()
+
+    print("All data deleted.")
+
+
+def change_color(color):
+    """
+    Function to change color.
+    """
+    if color.lower() in [
+        "black",
+        "red",
+        "green",
+        "yellow",
+        "blue",
+        "magenta",
+        "cyan",
+        "white",
+    ]:
+        with open(DATA_FILE, "r") as file:
+            data = json.load(file)
+
+        data["color"] = color.lower()
+        with open(DATA_FILE, "w") as file:
+            json.dump(data, file)
+        print(f"Color changed to '{color.lower()}'.")
+    else:
+        print(
+            "Invalid color. Available colors are: black, red, green, yellow, blue, magenta, cyan, white."
+        )
+
+
+# Configuración del analizador de argumentos de línea de comandos
+
+parser = argparse.ArgumentParser(description="Fetch on Windows")
+
+parser.add_argument("-d", "--delete", help="Delete the data", action="store_true")
+parser.add_argument(
+    "-c",
+    "--color",
+    metavar="color",
+    help="Change the color",
+    type=str,
 )
 
-print(
-    f"""
-{user_plus_machine}
-{"-" * 26}
-{Fore.RED}OS{Style.RESET_ALL}:\t {os_info}
-{Fore.RED}Uptime{Style.RESET_ALL}:  {get_uptime_info()}
-{Fore.RED}CPU{Style.RESET_ALL}:\t {cpuInfo}
-{Fore.RED}RAM{Style.RESET_ALL}:\t {Ram_info}                              
-{Fore.RED}Date{Style.RESET_ALL}:\t {date_today}
-{Fore.RED}Xq{Style.RESET_ALL}:\t {VERSION}
+args = parser.parse_args()
 
-{calendar_month}                                                                                                                                        
-{Style.DIM}{''.join(get_color_items())}
-{Style.BRIGHT}{''.join(get_color_items())}
+# Ejecutando funciones basadas en los argumentos proporcionados
 
+if args.delete:
+    delete_data()
 
-"""
-)
+if args.color:
+    change_color(args.color)
+
+# Imprimiendo información del sistema
+
+if __name__ == "__main__" and not (args.delete or args.color):
+    print(fetch_info())
--- test2.py
+++ test2.py
@@ -1,53 +1,37 @@
-#!/usr/bin/env python3
-
-from os import system
-from os import name as os_name
-from sys import platform as sys_platform
-from socket import gethostname as hostname
-from platform import platform as os_platform
-from platform import machine as machine_platform
-from cpuinfo import get_cpu_info as cpu_info
-from getpass import getuser
-
-from psutil import virtual_memory, boot_time
-from datetime import datetime, date
-from ascii import windows, linux, unknown
-
-LOVEFETCH_VERSION = "1.0.0"
-
-userName = getuser()
-hostName = hostname()
-osPlatform = os_platform()
-machinePlatform = machine_platform()
-cpuInfo = cpu_info()["brand_raw"]
-memory = round(virtual_memory().total / (1024.0**3), 2)
-dateToday = date.today()
-upTime = datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+import argparse
 
 
-def clearTerminal():
-    system("cls" if os_name == "nt" else "clear")
-
-
-def lovefetch():
-    clearTerminal()
-
-    if sys_platform.startswith("win"):
-        print(windows)
-    elif sys_platform.startswith("linux"):
-        print(linux)
+def imprimir_color(color):
+    colores = {
+        "rojo": "\033[91m",
+        "verde": "\033[92m",
+        "azul": "\033[94m",
+        "amarillo": "\033[93m",
+        "morado": "\033[95m",
+        "cyan": "\033[96m",
+        "blanco": "\033[97m",
+    }
+    if color in colores:
+        print(colores[color] + "Este es el color " + color + "!" + "\033[0m")
     else:
-        print(unknown)
-
-    print(f"[bold magenta] Username\t\t~> {userName} [/bold magenta]")
-    print(f"[bold magenta] Hostname\t\t~> {hostName} [/bold magenta]")
-    print(f"[bold magenta] Date\t\t\t~> {dateToday} [/bold magenta]")
-    print(f"[bold magenta] OS\t\t\t~> {osPlatform} [/bold magenta]")
-    print(f"[bold magenta] Uptime\t\t\t~> {upTime} [/bold magenta]")
-    print(f"[bold magenta] Memory\t\t\t~> {memory} GB [/bold magenta]")
-    print(f"[bold magenta] CPU\t\t\t~> {cpuInfo} [/bold magenta]")
-    print(f"[bold magenta] Machine Platform\t~> {machinePlatform} [/bold magenta]\n")
-    print(f"[bold green] Lovefetch version: {LOVEFETCH_VERSION} [/bold green]")
+        print("Color no válido")
 
 
-lovefetch()
+def main():
+    parser = argparse.ArgumentParser(
+        description="Imprime un mensaje en el color especificado."
+    )
+    parser.add_argument(
+        "-c",
+        "--color",
+        metavar="color",
+        type=str,
+        help="Especifica el color a imprimir",
+    )
+    args = parser.parse_args()
+    if args.color:
+        imprimir_color(args.color)
+
+
+if __name__ == "__main__":
+    main()
